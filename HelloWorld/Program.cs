fe6e6aa9ece6995aeffba0df9e951cdfa9c0d0a4
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2025-09-15 11:01:10 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+string greeting = "Hello, World!";
+
+Console.WriteLine(greeting);
+Console.WriteLine("Second comment");
+
+int number = 23;
+double decNumber = 22.2;
+
+Console.WriteLine(number);
+
+number = 10;
+
+Console.WriteLine(number);
+*/
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
+
+
+Console.WriteLine("enter a number");
+string input = Console.ReadLine();
+double number1 = Convert.ToDouble(input);
+
+Console.WriteLine("enter another number");
+input = Console.ReadLine();
+double number2 = Convert.ToDouble(input);
+
+Console.WriteLine(number1 + number2);
+
+
+
+
+
+
+
+
